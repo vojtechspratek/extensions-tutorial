9568f5ded62db3119d6822e91108093c8977d544
vojtechspratek <96978714+vojtechspratek@users.noreply.github.com>
2023-05-29 20:36:19 +0200

    Aktualizovat main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
-})
+namespace screenMagic{
+    export function plotAt(index: number): void{
+        index |= 0
+        const y = Math.floor(index / 5);
+        const x = Math.floor(index % 5);
+        led.plot(x, y)
+    }
+
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
-// Tady jsou testy. Při použití tohoto balíčku jako rozšíření nebude zkompilováno.
+for(let i = 0; i<25; i++){
+    screenMagic.plotAt(i)
+    basic.pause(500)
+    
+}
