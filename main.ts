49862974fcd6a827e1d9a0606492dc842252a1a4
vojtechspratek <96978714+vojtechspratek@users.noreply.github.com>
2023-05-29 20:39:31 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 namespace screenMagic{
+    /*
+        Address LEDs lineraly row first
+     */
+    //% blockId=screenmagicplotat
+    //% block="plit at index"
+    //% index.min=0 index.max=25
     export function plotAt(index: number): void{
         index |= 0
         const y = Math.floor(index / 5);
